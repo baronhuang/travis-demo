913cabcc6ed27cea7b0e2d7e76dd9c5e341bb22d
H008206 <huangbingliang@wtoip.com>
2020-04-10 10:18:36 +0800

    feat: first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // 这个 base 如果是部署在 github pages 需要设置，官方文档已经说明了
+  base: '/travis-demo/',
+  // 标题名字
+  title: "travis-demo",
+  // 描述
+  description: 'travis的配置demo，自动化打包、部署到github_pages',
+  // 构建后目录
+  dest: './dist'
+}
